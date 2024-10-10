a3c32b6a3716720ab53f0437b049227c30e370ec
Runrong Liu <lrrong2020@gmail.com>
2024-10-10 17:28:56 +0800

    try to visualize geolife.mat

---
+++ visualize.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.io as io
+
+# Assuming 'data' is a list of arrays, where each array is a trajectory
+# Each trajectory should be an array of shape (n_points, 2) where columns are latitude and longitude
+
+# Load your data
+raw_mat = io.loadmat("./datasets/geolife.mat")
+data = raw_mat['data'][0]  # Assuming data is structured this way
+
+# Plot each trajectory
+plt.figure(figsize=(10, 10))
+for trajectory in data:
+    plt.plot(trajectory[:, 1], trajectory[:, 0], label='Trajectory')  # longitude is x, latitude is y
+
+plt.xlabel('Longitude')
+plt.ylabel('Latitude')
+plt.title('Trajectory Data')
+plt.grid(True)
+plt.show()
