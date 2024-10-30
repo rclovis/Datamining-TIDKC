2c62c9f614772914b266705c83d29092ca09e06b
rclovis <clovis.rabot@epitech.eu>
2024-10-30 21:06:20 +0800

    IDK square working and refactor entire IDK code

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ import matplotlib.pyplot as plt
 plt.style.use('ggplot')
 
 if __name__ == "__main__":
-    raw_mat = io.loadmat("./datasets/CASIA.mat")
+    raw_mat = io.loadmat("./datasets/cross.mat")
     data = np.array(raw_mat['data'][0])
-    data = data[:100]
+    # data = data[:100]
 
     idk = IDK(random_seed=42)
-    result = idk.idk(data)
-
+    result = idk.idk_square(data)
     colors = np.array(["#000000", "#43cc5c"])
+
     for i in range(len(data)):
         plt.plot(data[i][:, 0], data[i][:, 1], color=plt.cm.jet(result[i]))
     plt.show()
