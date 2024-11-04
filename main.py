2e93b0bc3ece232d4ea95ebc7bea327f65fbf0a6
rclovis <clovis.rabot@epitech.eu>
2024-11-04 14:33:55 +0800

    Working DTW, hausdorff

--- main.py
+++ main.py
@@ -21,7 +21,9 @@ import TrajClustering as tc
 
 if __name__ == "__main__":
     traj = tc.TrajClustering()
-    traj.run_distance('geolife', 'IDK')
+    traj.load_dataset('geolife')
+    traj.plot_ground_truth()
+    traj.run_distance('EMD')
     traj.plot_mds()
     traj.run_clustering('Spectral', 10)
     traj.plot_clusters()
