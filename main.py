784b536fb2828239d6441fdb7bfbfd7e033e1362
rclovis <clovis.rabot@epitech.eu>
2024-11-13 16:56:15 +0800

    nmi and ari working

--- main.py
+++ main.py
@@ -9,20 +9,17 @@ import TrajClustering as tc
 #     raw_mat = io.loadmat("./datasets/cross.mat")
 #     data = np.array(raw_mat['data'][0])
 #     # data = data[:100]
-
 #     idk = IDK(random_seed=42)
 #     result = idk.idk_square(data)
 #     colors = np.array(["#000000", "#43cc5c"])
-
 #     for i in range(len(data)):
 #         plt.plot(data[i][:, 0], data[i][:, 1], color=plt.cm.jet(result[i]))
 #     plt.show()
-
 
 if __name__ == "__main__":
     traj = tc.TrajClustering()
     traj.load_dataset('geolife')
     traj.run_distance('IDK2')
     traj.plot_mds()
-    # traj.run_clustering('KMeans', 10)
-    # traj.plot_clusters()
+    traj.run_clustering('Spectral', 10)
+    traj.plot_clusters()
